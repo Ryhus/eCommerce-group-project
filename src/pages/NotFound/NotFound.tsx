20a43752c0915eb41d2534e577957ca8899f2458
Ryhus <ryhusyevhen@gmail.com>
2025-05-25 14:48:49 +0200

    feat(404 page): change href redirection to navigate(), change html tags to common components

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,15 +1,18 @@
 import "./NotFound.scss";
+import Button from "../../components/common/button/button";
+import { H1 } from "../../components/common/headings/H1";
+import Paragraph from "../../components/common/paragraph/paragraph";
+import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
+  const navigate = useNavigate();
   return (
     <div className="error-page">
       <div className="error-content">
-        <h1 className="error-code">404</h1>
-        <p className="error-message">Page not found</p>
-        <p className="error-description">The page you are looking for does not exist.</p>
-        <button className="home-button" onClick={() => (window.location.href = "/")}>
-          Go to homepage
-        </button>
+        <H1 className="error-code" text="404"></H1>
+        <Paragraph className="error-message" text="Page not found"></Paragraph>
+        <Paragraph className="error-description" text="The page you are looking for does not exist."></Paragraph>
+        <Button className="home-button" text="Go to homepage" onClick={() => navigate("/")}></Button>
       </div>
     </div>
   );
