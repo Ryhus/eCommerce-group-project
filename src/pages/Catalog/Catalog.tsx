44ae360b4763ef05445a961080541a8759eb01b1
n-andr <150958639+n-andr@users.noreply.github.com>
2025-05-30 00:45:52 +0200

    feat: add productServise api

--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -2,57 +2,88 @@ import ProductCard from "../../components/productCard/productCard";
 import "./Catalog.scss";
 import { H3 } from "../../components/common/headings/H3";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { Product } from "../../services/productServise/types";
+import { fetchProducts } from "../../services/productServise/productService";
 
 /*--- Mock products to be deleted after connecting to api---*/
 
-type Product = {
-  id: string;
-  name: string;
-  //onClick: () => void;
-  imgUrl: string;
-  currentPrice: string; // tbc if number or string
-  oldPrice: string;
-  altText?: string;
-  className?: string;
-};
+// type Product = {
+//   id: string;
+//   name: string;
+//   imgUrls: string[];
+//   currentPrice: string;
+//   oldPrice: string;
+// };
 
-const MOCK_PRODUCTS: Product[] = [
-  {
-    id: "shirt-001",
-    name: "T-shirt with Tape Details",
-    currentPrice: "120",
-    oldPrice: "120",
-    imgUrl:
-      "https://images.unsplash.com/photo-1581655353564-df123a1eb820?q=80&w=2112&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: "shirt-002",
-    name: "Graphic Tee",
-    currentPrice: "85",
-    oldPrice: "120",
-    imgUrl:
-      "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=2367&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-];
+// const MOCK_PRODUCTS: Product[] = [
+//   {
+//     id: "shirt-001",
+//     name: "T-shirt with Tape Details",
+//     currentPrice: "120",
+//     oldPrice: "120",
+//     imgUrls: [
+//       "https://images.unsplash.com/photo-1581655353564-df123a1eb820?q=80&w=2112&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     ],
+//   },
+//   {
+//     id: "shirt-002",
+//     name: "Graphic Tee",
+//     currentPrice: "85",
+//     oldPrice: "120",
+//     imgUrls: [
+//       "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?q=80&w=2367&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//     ],
+//   },
+// ];
 
 /*--- end mock products ---*/
 
 export default function CatalogPage() {
   const navigate = useNavigate();
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  //   const [error] = useState<string | null>(null);
+
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function load() {
+      try {
+        const data = await fetchProducts();
+        setProducts(data);
+        //   } catch (err: any) {
+        //     setError(err.message || "Failed to load products");
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else setError(String(err));
+      } finally {
+        setLoading(false);
+      }
+    }
+    load();
+  }, []);
+
+  if (loading) return <div>Loading list of products…</div>; //later: consider scenario with a lot of products and slow loading
+  if (error) return <div>Error: {error}</div>; //later: style error scenario
+
   return (
     <div>
       <H3 text="Catalog" />
-      <div className="product-list">
-        {MOCK_PRODUCTS.map((item) => (
-          <ProductCard
-            name={item.name}
-            currentPrice={item.currentPrice}
-            oldPrice={item.oldPrice}
-            imgUrl={item.imgUrl}
-            onClick={() => navigate(`products/${item.id}`)}
-          />
-        ))}
-      </div>
+      {
+        <div className="product-list">
+          {products.map((item) => (
+            <ProductCard
+              name={item.name}
+              currentPrice={item.currentPrice}
+              oldPrice={item.oldPrice}
+              imgUrl={item.imgUrls[0]}
+              onClick={() => navigate(`products/${item.id}`)}
+            />
+          ))}
+        </div>
+      }
     </div>
   );
 }
---
+++ src/services/productServise/productService.ts
@@ -0,0 +1,33 @@
+import type { Product } from "./types";
+import { apiClient } from "../apiClient";
+
+const PROJECT_KEY = import.meta.env.VITE_CTP_PROJECT_KEY;
+
+export async function fetchProducts(): Promise<Product[]> {
+  const response = await apiClient.get<{
+    results: Array<{
+      id: string;
+      name: { en: string };
+      masterVariant: {
+        images: { url: string }[];
+        prices: Array<{
+          value: { centAmount: number };
+          discounted?: { value: { centAmount: number } };
+        }>;
+      };
+    }>;
+  }>(`/${PROJECT_KEY}/product-projections/search`);
+  return response.data.results.map((item) => {
+    const currentPriceiInCents =
+      item.masterVariant.prices[0].discounted?.value.centAmount ?? item.masterVariant.prices[0].value.centAmount;
+    const oldPriceiInCents = item.masterVariant.prices[0].value.centAmount;
+
+    return {
+      id: item.id,
+      name: item.name.en,
+      imgUrls: item.masterVariant.images.map((img) => img.url),
+      currentPrice: (currentPriceiInCents / 100).toFixed(2),
+      oldPrice: (oldPriceiInCents / 100).toFixed(2),
+    };
+  });
+}
