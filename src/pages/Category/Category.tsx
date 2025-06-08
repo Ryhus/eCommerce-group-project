fa15f8747615f548c64e6d795eeb67a3d1709c57
Olha Teplova <olitieplove@gmail.com>
2025-06-08 16:13:37 +0200

    feat: add catagory view for anonymous client

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { TokenService } from "../../services/TokenService";
+import { AuthService } from "../../services/AuthService";
 import axios from "axios";
 
 import Breadcrumbs from "../../components/Breadcrumbs/Breadcrumbs";
@@ -38,6 +40,14 @@ export default function CategoryPage() {
 
   const rawPath = location.pathname.replace(/^\/catalog\/?/, "");
   const segments = rawPath === "" ? [] : rawPath.split("/");
+
+  if (!TokenService.getAccessToken()) {
+    const anonymousClient = async function () {
+      await AuthService.anonymousAuthenticate();
+    };
+
+    anonymousClient();
+  }
 
   useEffect(() => {
     setLoading(true);
