2b56e0f9a82d3b4af51eaca22441d4e9f3364bf3
Olha Teplova <olitieplove@gmail.com>
2025-06-22 17:33:43 +0200

    feat: add about-page and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,21 @@
-function Footer() {
+import "./Footer.scss";
+
+const Footer = () => {
   return (
-    <footer>
-      <p>This is footer</p>
+    <footer className="footer">
+      <div className="footer__content">
+        <a href="https://rs.school/" target="_blank" rel="noopener noreferrer" className="footer__logo-link">
+          <img src="/logos/rs-school-logo.svg" alt="RS School Logo" className="footer__logo" />
+        </a>
+
+        <p className="footer__year">© 2025</p>
+
+        <a href="https://github.com/ryhus" target="_blank" rel="noopener noreferrer" className="footer__logo-link">
+          <img src="/logos/gh-logo.png" alt="Github Logo" className="footer__logo" />
+        </a>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
