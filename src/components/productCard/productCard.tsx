122dc83f02d4af45c32af405c04801892151f25c
n-andr <150958639+n-andr@users.noreply.github.com>
2025-06-22 22:21:04 +0200

    fix: add placeholder if missing pictures in product card

--- src/components/productCard/productCard.tsx
+++ src/components/productCard/productCard.tsx
@@ -36,6 +36,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
   if (description && description.length > 50) {
     shortDescription = description.slice(0, 47).concat("...");
   }
+  if (imgUrl === "") imgUrl = "";
 
   const [productInCart, setProductInCart] = useState(false);
   const { cart, addToCart } = useCart();
@@ -49,7 +50,22 @@ const ProductCard: React.FC<ProductCardProps> = ({
   return (
     <div className={`product-card ${className}`} id={id} onClick={onClick}>
       <div className="product-card__img-wrapper">
-        <img src={imgUrl} alt={altText} className="product-card__img" />
+        {imgUrl ? (
+          <img src={imgUrl} alt={altText} className="product-card__img" />
+        ) : (
+          <svg
+            className="product-card__img-placeholder"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 344 405"
+            height="250"
+            fill="none"
+          >
+            <path
+              d="M277 389.5C271.579 306.33 270.559 255.236 271.5 158L285.5 176.5C310.635 169.356 323.107 162.776 342.5 146.5C321.936 95.5935 310.22 65.8516 294 43C269.282 28.8028 245.885 20.0256 211.5 5L207.5 1C178.568 14.3124 162.661 15.0307 135 1L131.5 5C90.2078 24.9976 66.2063 31.2995 55.5 43C42.5125 54.6489 29.7184 81.5541 1.5 149.5C36.0193 168.69 53.1848 177.231 71.5 180L83.5 158V392C95 407.5 268 408.5 277 389.5Z"
+              stroke="#888888"
+            />
+          </svg>
+        )}
       </div>
       <div className="product-card__info">
         <Paragraph text={name} className="product-card__name" />
