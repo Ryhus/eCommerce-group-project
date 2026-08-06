75c61abf2dd3f963d1d9590d83826b01cf4ab88c
Ryhus <ryhusyevhen@gmail.com>
2026-08-06 19:58:03 +0200

    feat(frontend): implement Span typography primitive

--- frontend/src/components/common/span/span.tsx
+++ frontend/src/components/common/span/span.tsx
@@ -1,1 +1,21 @@
-// span component here
+import type { ComponentPropsWithoutRef } from "react";
+
+import "./span.scss";
+
+type SpanProps = Omit<ComponentPropsWithoutRef<"span">, "children"> & {
+  text: string;
+  isError?: boolean;
+};
+
+const Span = ({ text, isError = false, className = "", ...props }: SpanProps) => {
+  const errorClass = isError ? "span--error" : "";
+  const spanClass = `span ${errorClass} ${className}`.trim();
+
+  return (
+    <span {...props} className={spanClass}>
+      {text}
+    </span>
+  );
+};
+
+export default Span;
