fc97224cda01b23e44c2e014660c2f218f867ca5
Ryhus <ryhusyevhen@gmail.com>
2026-08-06 18:14:34 +0200

    chore(frontend): add storefront font assets

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-500.css";
+import "@fontsource/roboto/latin-700.css";
 import "./assets/styles/global.scss";
 
 createRoot(document.getElementById("root")!).render(
