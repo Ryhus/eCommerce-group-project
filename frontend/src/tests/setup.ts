0a1453378cae45997de980bbd2765a535cd9c6af
Ryhus <ryhusyevhen@gmail.com>
2026-08-08 12:59:48 +0200

    feat(frontend): add language switcher to header

--- frontend/src/tests/setup.ts
+++ frontend/src/tests/setup.ts
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+import { beforeAll } from "vitest";
+
+import { i18nInitialization } from "../i18n/i18n";
+
+beforeAll(async () => {
+  await i18nInitialization;
+});
